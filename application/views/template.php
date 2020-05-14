fbacf6e34bce3a8de50bf94018645ed8a0cb0ac7
nest-code <ernestoandreyulian@gmail.com>
2020-05-15 01:57:38 +0700

    # fix bug template

--- application/views/template.php
+++ application/views/template.php
@@ -1,108 +1,103 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Blank Page</title>
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-  <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/Ionicons/css/ionicons.min.css">
-  <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/skins/_all-skins.min.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Pos | Point Of Sale</title>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<link rel="stylesheet" href="<?=base_url()?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/bower_components/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/bower_components/Ionicons/css/ionicons.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/dist/css/AdminLTE.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/dist/css/skins/_all-skins.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
+	<div class="wrapper">
+		<header class="main-header">
+			<a href="<?=base_url('dashboard')?>" class="logo">
+				<span class="logo-mini"><b>A</b>LT</span>
+				<span class="logo-lg"><b>Admin</b>LTE</span>
+			</a>
+			<nav class="navbar navbar-static-top">
+				<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
 
-  <header class="main-header">
-    <a href="<?=base_url('dashboard')?>" class="logo">
-      <span class="logo-mini"><b>A</b>LT</span>
-      <span class="logo-lg"><b>Admin</b>LTE</span>
-    </a>
-    <nav class="navbar navbar-static-top">
-      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </a>
+				<div class="navbar-custom-menu">
+					<ul class="nav navbar-nav">
+						<li class="dropdown tasks-menu">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								<i class="fa fa-flag-o"></i>
+								<span class="label label-danger">9</span>
+							</a>
+							<ul class="dropdown-menu">
+								<li class="header">You have 9 tasks</li>
+								<li>
+									<ul class="menu">
+										<a href="#">
+											<h3>
+												Design some buttons
+												<small class="pull-right">20%</small>
+											</h3>
+											<div class="progress xs">
+												<div class="progress-bar progress-bar-aqua" style="width: 20%"
+													role="progressbar" aria-valuenow="20" aria-valuemin="0"
+													aria-valuemax="100">
+													<span class="sr-only">20% Complete</span>
+												</div>
+											</div>
+										</a>
+								</li>
+							</ul>
+						</li>
+						<li class="footer">
+							<a href="#">View all tasks</a>
+						</li>
+					</ul>
+					</li>
+					<li class="dropdown user user-menu">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<img src="<?=base_url()?>assets//dist/img/user2-160x160.jpg" class="user-image"
+								alt="User Image">
+							<span class="hidden-xs"><?=$this->fungsis->user_login()->username ?></span>
+						</a>
+						<ul class="dropdown-menu">
+							<li class="user-header">
+								<img src="<?=base_url()?>assets//dist/img/user2-160x160.jpg" class="img-circle"
+									alt="User Image">
+								<p>
+									<?=$this->fungsis->user_login()->name ?>
+									<small><?=$this->fungsis->user_login()->address ?></small>
+								</p>
+							</li>
 
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-              
-          <li class="dropdown tasks-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-flag-o"></i>
-              <span class="label label-danger">9</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 9 tasks</li>
-              <li>
-                <ul class="menu">
-                    <a href="#">
-                      <h3>
-                        Design some buttons
-                        <small class="pull-right">20%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">20% Complete</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <!-- end task item -->
-                </ul>
-              </li>
-              <li class="footer">
-                <a href="#">View all tasks</a>
-              </li>
-            </ul>
-          </li>
-          <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="<?=base_url()?>assets//dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs"><?=$this->fungsis->user_login()->username ?></span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="user-header">
-                <img src="<?=base_url()?>assets//dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+							<li class="user-footer">
+								<div class="pull-left">
+									<a href="#" class="btn btn-default btn-flat">Profile</a>
+								</div>
+								<div class="pull-right">
+									<a href="<?=site_url('auth/logut')?>" class="btn btn-default btn-flat">Sign out</a>
+								</div>
+							</li>
+						</ul>
+					</li>
+					<li>
+					</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
 
-                <p>
-                <?=$this->fungsis->user_login()->name ?>
-                  <small><?=$this->fungsis->user_login()->address ?></small>
-                </p>
-              </li>
-    
-              <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
-                <div class="pull-right">
-                  <a href="<?=site_url('auth/logut')?>" class="btn btn-default btn-flat">Sign out</a>
-                </div>
-              </li>
-            </ul>
-          </li>
-          <!-- Control Sidebar Toggle Button -->
-          <li>
-            <!-- <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a> -->
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
 
-  <!-- =============================================== -->
-
-  <!-- Left side column. contains the sidebar -->
-  <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
+    <aside class="main-sidebar">
     <section class="sidebar">
-      <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
           <img src="<?=base_url()?>assets//dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
@@ -112,7 +107,6 @@
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
-      <!-- search form -->
       <form action="#" method="get" class="sidebar-form">
         <div class="input-group">
           <input type="text" name="q" class="form-control" placeholder="Search...">
@@ -122,8 +116,6 @@
               </span>
         </div>
       </form>
-      <!-- /.search form -->
-      <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
 
@@ -133,13 +125,11 @@
           </a>
         </li>
 
-
         <li <?=$this->uri->segment(1) == 'supplier' ? 'class="active"' : '' ?>>
           <a href="<?=site_url('supplier')?>">
             <i class="fa fa-truck"></i> <span>Suplier</span>
           </a>
-        </li>
-        
+        </li>  
 
         <li <?=$this->uri->segment(1) == 'costumer' ? 'class="active"' : '' ?>>
           <a href="<?=site_url('costumer')?>">
@@ -151,7 +141,6 @@
           <a href="">
             <i class="fa fa-shopping-cart"></i>
             <span>Product</span>
-
             </span>
           </a>
           <ul class="treeview-menu">
@@ -161,13 +150,10 @@
               </ul>
         </li>
 
-
-
         <li class="treeview <?=$this->uri->segment(1) == 'sale' || $this->uri->segment(1) == 'stock_in' || $this->uri->segment(1) == 'stock_out'   ? 'active' : '' ?>">
           <a href="#">
             <i class="fa fa-shopping-cart"></i>
             <span>Transection</span>
-
             </span>
           </a>
           <ul class="treeview-menu">
@@ -191,7 +177,6 @@
         </li>
                     
 
-        <?php //if($this->session->userdata('level') == 1) { ?>
         <li class="treeview active">
         <li class="header">MAIN NAVIGATION</li>   
 
@@ -202,172 +187,46 @@
 
     </section>
   </aside>
-
-<script src="<?=base_url()?>assets/bower_components/jquery/dist/jquery.min.js"></script>
-        
-  <div class="content-wrapper">
-    <?php echo $contents ?>
-  </div>
-
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-    reserved.
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Create the tabs -->
-    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-      <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-
-      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-    </ul>
-    <!-- Tab panes -->
-
-      <!-- Home tab content -->
-      <div class="tab-pane" id="control-sidebar-home-tab">
-        <h3 class="control-sidebar-heading">Recent Activity</h3>
-        <ul class="control-sidebar-menu">
-          <li>
-            <a href="javascript:void(0)">
-              <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
-                <p>Will be 23 on April 24th</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <i class="menu-icon fa fa-user bg-yellow"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
-
-                <p>New phone +1(800)555-1234</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
-
-                <p>nora@example.com</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <i class="menu-icon fa fa-file-code-o bg-green"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
-
-                <p>Execution time 5 seconds</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!-- /.control-sidebar-menu -->
-
-        <h3 class="control-sidebar-heading">Tasks Progress</h3>
-        <ul class="control-sidebar-menu">
-          <li>
-            <a href="javascript:void(0)">
-              <h4 class="control-sidebar-subheading">
-                Custom Template Design
-                <span class="label label-danger pull-right">70%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <h4 class="control-sidebar-subheading">
-                Update Resume
-                <span class="label label-success pull-right">95%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-success" style="width: 95%"></div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <h4 class="control-sidebar-subheading">
-                Laravel Integration
-                <span class="label label-warning pull-right">50%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <h4 class="control-sidebar-subheading">
-                Back End Framework
-                <span class="label label-primary pull-right">68%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!-- /.control-sidebar-menu -->
-
-      </div>
-      <!-- /.tab-pane -->
-      <!-- Stats tab content -->
-      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-      <!-- /.tab-pane -->
-      <!-- Settings tab content -->
     
-      <!-- /.tab-pane -->
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
-  <!-- Add the sidebar's background. This div must be placed
-       immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
-</div>
-<!-- ./wrapper -->
+		<script src="<?=base_url()?>assets/bower_components/jquery/dist/jquery.min.js"></script>
 
-<script src="<?=base_url()?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="<?=base_url()?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="<?=base_url()?>assets/bower_components/fastclick/lib/fastclick.js"></script>
-<script src="<?=base_url()?>assets/dist/js/adminlte.min.js"></script>
-<script src="<?=base_url()?>assets/dist/js/demo.js"></script>
-<script src="<?=base_url()?>assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+		<div class="content-wrapper">
+			<?php echo $contents ?>
+		</div>
 
-<script>
-  $(document).ready(function () {
-    $('#tabel1').dataTable()
-  })
-</script>
+		<footer class="main-footer">
+			<div class="pull-right hidden-xs">
+				<b>Version</b> 2.4.0
+			</div>
+			<strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Nest Code</a>.</strong> All rights
+			reserved.
+		</footer>
 
-<script>
-  $(document).ready(function () {
-    $('.sidebar-menu').tree()
-  })
-</script>
+
+	<div class="control-sidebar-bg"></div>
+
+	<script src="<?=base_url()?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="<?=base_url()?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="<?=base_url()?>assets/bower_components/fastclick/lib/fastclick.js"></script>
+	<script src="<?=base_url()?>assets/dist/js/adminlte.min.js"></script>
+	<script src="<?=base_url()?>assets/dist/js/demo.js"></script>
+	<script src="<?=base_url()?>assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+
+	<script>
+		$(document).ready(function () {
+			$('#tabel1').dataTable()
+		})
+
+	</script>
+
+	<script>
+		$(document).ready(function () {
+			$('.sidebar-menu').tree()
+		})
+	</script>
 
 
 </body>
+
 </html>
